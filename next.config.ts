6456843cb44a24c30b402a908a306bf88fb9f522
amr <alikhaledomar8@gmail.com>
2025-12-06 05:04:28 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
+const isGitHubPages = !isVercel && isProd;
 
 module.exports = {
-  output: 'export', // static HTML export
-  basePath: isProd ? '/ali-portfolio-next' : '', // your GitHub repo name
-  assetPrefix: isProd ? '/ali-portfolio-next/' : '',
+  // Remove static export on Vercel
+  output: isGitHubPages ? 'export' : undefined,
+  basePath: isGitHubPages ? '/ali-portfolio-next' : '',
+  assetPrefix: isGitHubPages ? '/ali-portfolio-next/' : '',
 };
